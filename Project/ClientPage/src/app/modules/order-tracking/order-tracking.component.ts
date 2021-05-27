75ef6d6632d5875f8bc0a4608014768d110e508a
Truc Map <huynhhuetruc@gmail.com>
2021-05-27 11:53:47 +0700

    Update

--- Project/ClientPage/src/app/modules/order-tracking/order-tracking.component.ts
+++ Project/ClientPage/src/app/modules/order-tracking/order-tracking.component.ts
@@ -89,21 +89,22 @@ export class OrderTrackingComponent implements OnInit {
           this.dsdanhgia.push(this.lsthoadonbanhang[i])
         }
       }
-
       this.getdssanpham()
     })
   }
 
   getdssanpham() {
     let count = 0
+    let hinhanh: AnhDaiDienModel[] = []
+    let tmp: any
     this.sanphamService.getListSanPham().subscribe((res: any) => {
       this.dssanpham = res.sanphams;
 
       for (const i in this.dsdanhgia) {
         // Lấy ảnh đại diện cho đơn mua
-        this.sanphamdanhgia.push({SanPham_id: this.dsdanhgia[i].San_Pham[0].SanPham_id, Hinh_anh: ''})
-
         for (const j in this.dsdanhgia[i].San_Pham) {
+          hinhanh.push({ SanPham_id: this.dsdanhgia[i].San_Pham[j].SanPham_id, Hinh_anh: '', Ten_san_pham: '' })
+
           for (const k in this.dssanpham) {
             if (this.dsdanhgia[i].San_Pham[j].SanPham_id === this.dssanpham[k]._id) {
               for (const l in this.dssanpham[k].Danh_gia) {
@@ -115,53 +116,82 @@ export class OrderTrackingComponent implements OnInit {
           }
 
         }
+        tmp = hinhanh
+        this.sanphamdanhgia.push(tmp)
+        hinhanh = []
         this.arrdanhgia.push(count)
         count = 0
       }
+
       for (const n in this.arrdanhgia) {
         if (this.arrdanhgia[n] === 0) {
           this.soluongdanhgia += 1
         }
       }
 
-      for (const l in this.dschoxacnhan){
-        this.sanphamchoxacnhan.push({SanPham_id: this.dschoxacnhan[l].San_Pham[0].SanPham_id, Hinh_anh: ''})
+      for (const l in this.dschoxacnhan) {
+        for (const k in this.dschoxacnhan[l].San_Pham) {
+          hinhanh.push({ SanPham_id: this.dschoxacnhan[l].San_Pham[k].SanPham_id, Hinh_anh: '', Ten_san_pham: '' })
+        }
+        tmp = hinhanh
+        this.sanphamchoxacnhan.push(tmp)
+        hinhanh = []
       }
 
-      for (const k in this.dscholayhang){
-        this.sanphamcholayhang.push({SanPham_id: this.dscholayhang[k].San_Pham[0].SanPham_id, Hinh_anh: ''})
+      for (const k in this.dscholayhang) {
+        for (const l in this.dscholayhang[k].San_Pham) {
+          hinhanh.push({ SanPham_id: this.dscholayhang[k].San_Pham[l].SanPham_id, Hinh_anh: '', Ten_san_pham: '' })
+        }
+        tmp = hinhanh
+        this.sanphamcholayhang.push(tmp)
+        hinhanh = []
       }
 
-      for (const m in this.dsdanggiaohang){
-        this.sanphamdanggiaohang.push({SanPham_id: this.dsdanggiaohang[m].San_Pham[0].SanPham_id, Hinh_anh: ''})
+      for (const m in this.dsdanggiaohang) {
+        for (const l in this.dsdanggiaohang[m].San_Pham) {
+          hinhanh.push({ SanPham_id: this.dsdanggiaohang[m].San_Pham[l].SanPham_id, Hinh_anh: '', Ten_san_pham: '' })
+        }
+        tmp = hinhanh
+        this.sanphamdanggiaohang.push(tmp)
+        hinhanh = []
       }
 
-      for (const j in this.dssanpham){
-        for (const h in this.sanphamdanhgia){
-          if(this.sanphamdanhgia[h].SanPham_id === this.dssanpham[j]._id){
-            this.sanphamdanhgia[h].Hinh_anh = this.dssanpham[j].Hinh_anh
+      for (const j in this.dssanpham) {
+        for (const h in this.sanphamdanhgia) {
+          for (const k in this.sanphamdanhgia[h]) {
+            if (this.sanphamdanhgia[h][k].SanPham_id === this.dssanpham[j]._id) {
+              this.sanphamdanhgia[h][k].Hinh_anh = this.dssanpham[j].Hinh_anh
+              this.sanphamdanhgia[h][k].Ten_san_pham = this.dssanpham[j].Ten_san_pham
+            }
+          }
+
+        }
+        for (const h in this.sanphamchoxacnhan) {
+          for (const k in this.sanphamchoxacnhan[h]) {
+            if (this.sanphamchoxacnhan[h][k].SanPham_id === this.dssanpham[j]._id) {
+              this.sanphamchoxacnhan[h][k].Hinh_anh = this.dssanpham[j].Hinh_anh
+              this.sanphamchoxacnhan[h][k].Ten_san_pham = this.dssanpham[j].Ten_san_pham
+            }
           }
         }
-        for (const h in this.sanphamchoxacnhan){
-          if(this.sanphamchoxacnhan[h].SanPham_id === this.dssanpham[j]._id){
-            this.sanphamchoxacnhan[h].Hinh_anh = this.dssanpham[j].Hinh_anh
+        for (const h in this.sanphamcholayhang) {
+          for (const k in this.sanphamcholayhang[h]) {
+            if (this.sanphamcholayhang[h][k].SanPham_id === this.dssanpham[j]._id) {
+              this.sanphamcholayhang[h][k].Hinh_anh = this.dssanpham[j].Hinh_anh
+              this.sanphamcholayhang[h][k].Ten_san_pham = this.dssanpham[j].Ten_san_pham
+            }
           }
         }
-        for (const h in this.sanphamcholayhang){
-          if(this.sanphamcholayhang[h].SanPham_id === this.dssanpham[j]._id){
-            this.sanphamcholayhang[h].Hinh_anh = this.dssanpham[j].Hinh_anh
-          }
-        }
-        for (const h in this.sanphamdanggiaohang){
-          if(this.sanphamdanggiaohang[h].SanPham_id === this.dssanpham[j]._id){
-            this.sanphamdanggiaohang[h].Hinh_anh = this.dssanpham[j].Hinh_anh
+        for (const h in this.sanphamdanggiaohang) {
+          for (const k in this.sanphamdanggiaohang) {
+            if (this.sanphamdanggiaohang[h][k].SanPham_id === this.dssanpham[j]._id) {
+              this.sanphamdanggiaohang[h][k].Hinh_anh = this.dssanpham[j].Hinh_anh
+              this.sanphamdanggiaohang[h][k].Ten_san_pham = this.dssanpham[j].Ten_san_pham
+            }
           }
         }
       }
-      // console.log(this.sanphamdanhgia)
-      // console.log(this.sanphamchoxacnhan)
-      // console.log(this.sanphamcholayhang)
-      // console.log(this.sanphamdanggiaohang)
+
     })
   }
 
@@ -172,7 +202,7 @@ export class OrderTrackingComponent implements OnInit {
     return true
   }
 
-  ChuyenTrangBinhLuan(eachPhieuDat){
+  ChuyenTrangBinhLuan(eachPhieuDat) {
     this.router.navigateByUrl(`/comment/${eachPhieuDat._id}`);
   }
 
--- Project/ClientPage/src/models/SanPham/anhdaidien.ts
+++ Project/ClientPage/src/models/SanPham/anhdaidien.ts
@@ -1,4 +1,5 @@
 export class AnhDaiDienModel{
   SanPham_id: string;
+  Ten_san_pham: string;
   Hinh_anh: string;
 }
